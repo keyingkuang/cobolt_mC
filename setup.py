66969a8653b7c2342df27a758f937f6eb124c8dd
Maggie Kuang <keying_kuang@berkeley.edu>
2022-08-19 11:29:45 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="cobolt", # Replace with your own username
-    version="0.0.1",
-    author="boyinggong",
-    author_email="boyinggong@berkeley.edu",
-    description="A package for joint analysis of multimodal single-cell sequencing data",
+setup(
+    name='cobolt',
+    version='0.0.1',
+    author='boyinggong',
+    author_email='boyinggong@berkeley.edu',
+    description='A package for joint analysis of multimodal single-cell sequencing data',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/boyinggong/cobolt",
@@ -20,16 +17,18 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'scipy',
+        'pandas',
         'torch',
         'umap-learn',
         'python-igraph',
         'sklearn',
-        'xgboost'
+        'xgboost',
+        'seaborn',
+        'leidenalg',
     ],
-    package_dir={"": "cobolt"},
-    packages=setuptools.find_packages(where=""),
-    python_requires=">=3.7",
+    python_required=">=3.7"
 )
